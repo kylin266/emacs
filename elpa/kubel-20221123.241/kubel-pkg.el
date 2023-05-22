319be01dd6cd9796d4c4a5def845fb416ed43b89
totuan <tuanto002@gmail.com>
2023-05-22 14:12:16 +0700

    Initial commit

---
+++ elpa/kubel-20221123.241/kubel-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from kubel.el  -*- no-byte-compile: t -*-
+(define-package "kubel" "20221123.241" "Control Kubernetes with limited permissions" '((transient "0.1.0") (emacs "25.3") (dash "2.12.0") (s "1.2.0") (yaml-mode "0.0.14")) :commit "5962d566db17ddfa25ac3120b4591df1d741e979" :authors '(("Adrien Brochard")) :maintainer '("Adrien Brochard") :keywords '("kubernetes" "k8s" "tools" "processes") :url "https://github.com/abrochard/kubel")
